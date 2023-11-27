4c78da5c20c41aea6a044120ad6f3bdbffbef179
NicholasVB <tivalik2@gmail.com>
2023-11-27 17:42:45 +0200

    Implemented fetching TODO list.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+async function getTodos (url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getTodos('https://jsonplaceholder.typicode.com/todos?_limit=20');
+        setTodoList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+    
+  }, []);
+
+  
+
   return (
     <div>
-      
+      {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
     </div>
   );
 }
