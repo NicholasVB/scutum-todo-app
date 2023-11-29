bc65d5ddd84bcff5983f71dc9058a42df04d538f
NicholasVB <tivalik2@gmail.com>
2023-11-29 18:36:56 +0200

    Update  todoItem: added dispatch, actions, structure

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,6 +1,29 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { DeleteSharp, AddSharp } from '@mui/icons-material';
+import { useDispatch } from 'react-redux';
+import { addTodo, updateTodo, deleteTodo } from '../../redux/slices/todosList';
 
-export function TodoItem ({key, title}) {
+
+export function TodoItem ({id, title="", isDelete=true}) {
+    // const [todoText, setTodoText] = useState(title);
+    const dispatch = useDispatch();
     return(
-        <li key={key}>{title}</li>
+        <Box key={id}>
+            <TextField 
+                id="standard-basic" 
+                // value={todoText}
+                value={title}
+                onChange={(event) => { dispatch(updateTodo({ todoId: id, newText: event.target.value })) }} 
+                // onChange={(event) => { setTodoText(event.target.value) }} 
+            />
+            {/* {isDelete 
+                ?  */}
+                <DeleteSharp onClick={() => {dispatch(deleteTodo(id))}} />
+            {/* //     : <AddSharp onClick={(event) => {dispatch(addTodo({ todoId: id, newText: event.target.value }))}} />
+            // }  */}
+            
+        </Box>
     )
 }
