6680b94493489a3464ddd5a67b8e6c0524e3b81d
NicholasVB <tivalik2@gmail.com>
2023-12-02 09:51:00 +0200

    Created: index.js file in folder components for convenient import components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { AddNewTodo } from "./AddNewTodo/AddNewTodo";
+export { Icon } from "./Icon/Icon";
+export { Pagination } from "./Pagination/Pagination";
+export { TodoItem } from "./TodoItem/TodoItem";
+export { TodoList } from "./TodoList/TodoList";
